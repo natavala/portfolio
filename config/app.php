a0ce6b1969e999c31fa905c59d239e6a02e8878b
Nathalie Vallieres <vallieres.nathalie@gmail.com>
2020-02-24 22:08:12 -0500

    feat(French) : Added French routes

--- config/app.php
+++ config/app.php
@@ -225,6 +225,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // Custom class aliases
+        'Project' => App\Project::class,
+        'Page' => App\Page::class
 
     ],
 
