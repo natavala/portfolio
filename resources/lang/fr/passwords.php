ebdfc5f639b05c7ab1c082a92422b6d5ca0bde6b
Nathalie Vallieres <vallieres.nathalie@gmail.com>
2020-02-24 22:07:30 -0500

    feat(French) : Added French language files

---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous avons envoyer un lien de réinitialisation par courriel!',
+    'throttled' => 'Veuillez attendre avant de réessayer.',
+    'token' => 'La clé de réinitialisation du mot de passe est invalide.',
+    'user' => "Nous n'avons pas trouvé d'utilisateur avec cette adresse courriel.",
+
+];
---
+++ resources/lang/fr/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default routes. 
+    |
+    */
+
+    'home' => 'accueil',
+    'projects' => 'projets'
+];
